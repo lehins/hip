f14624d5938e39080dd354e203aecf5f32643308
Alexey Kuleshevich <lehins@yandex.ru>
2015-11-18 21:24:40 -0700

    added images for demo

--- Main.hs
+++ Main.hs
@@ -6,7 +6,9 @@ import qualified Graphics.Image.Repa.Parallel as R
   
 main :: IO ()  
 main = do
-  frog <- R.readImageRGBA "frog.jpg"
-  R.writeImage "frog_mod.jpg" frog []
-  --R.displayImage frog
+  frog <- R.readImageRGBA "images/downloaded/frog-640x412.jpg"
+  centaurus <- R.readImageRGBA "images/downloaded/centaurus-galaxy.jpg"
+  cluster <- R.readImageRGBA "images/downloaded/star-cluster.jpg"  
+  --R.writeImage "frog_mod.jpg" frog []
+  R.displayImage cluster
   return ()
