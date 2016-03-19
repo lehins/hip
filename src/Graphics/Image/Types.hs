52397a8afa17584205f250a619978baf1f63ffe0
Alexey Kuleshevich <lehins@yandex.ru>
2016-03-18 20:15:21 -0600

    added image file formats to Graphics.Image.Types

--- src/Graphics/Image/Types.hs
+++ src/Graphics/Image/Types.hs
@@ -8,9 +8,10 @@
 --
 module Graphics.Image.Types (
   module Graphics.Image.ColorSpace,
+  module Graphics.Image.IO.External,
   Array, Image, ManifestArray, SequentialArray, MutableArray, MImage,
   Exchangable, Border(..),
-  VU(..), RD(..), RS(..), RP(..), 
+  VU(..), RD(..), RS(..), RP(..),
   ) where
 
 
@@ -18,3 +19,4 @@ import Graphics.Image.ColorSpace
 import Graphics.Image.Interface
 import Graphics.Image.Interface.Vector (VU(..))
 import Graphics.Image.Interface.Repa (RD(..), RS(..), RP(..))
+import Graphics.Image.IO.External
