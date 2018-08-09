c60b587502836653afc70d2c86fd60a18a234287
Khilan Ravani <khilanravani@gmail.com>
2018-08-09 18:48:48 +0530

    S&P Noise PR Corrections 1

--- src/Graphics/Image/Processing/Noise.hs
+++ src/Graphics/Image/Processing/Noise.hs
@@ -13,19 +13,18 @@ import Prelude as P hiding (subtract)
 import Graphics.Image.Interface as I
 import Graphics.Image
 import Graphics.Image.Types as IP
-import Graphics.Image.ColorSpace (X)
 
 -- | Helper function for generating a list of random co-ordinates.
 randomCoords :: StdGen -> Int -> Int -> [(Int,Int)]
 randomCoords a width height = (rnx1, rny1) : randomCoords g2 width height
   where
-    (rnx1, g1) = randomR (1, width) a
-    (rny1, g2) = randomR (1, height) g1
+    (rnx1, g1) = randomR (0, width) a
+    (rny1, g2) = randomR (0, height) g1
 
 -- | Salt and pepper noise or impulse noise is a form of noise seen on images.
 -- It is mainly caused by sharp and sudden disturbances in the image signal. 
 -- 
--- 'snp' generates this particular type of noise by introducing a sparse 
+-- 'saltAndPepper' generates this particular type of noise by introducing a sparse 
 -- distribution of white and black pixels in the input image. The level or intensity
 -- of the noise to be introduced is a parameter of this method and is scaled 
 -- between 0 and 1, that is the input Noise Intensity has a domain : (0, 1).
@@ -36,25 +35,19 @@ randomCoords a width height = (rnx1, rny1) : randomCoords g2 width height
 -- 
 -- >>> img <- readImageY VU "images/yield.jpg"
 -- >>> input1 <- getLine
--- >>> input2 <- getLine
--- >>> input3 <- getLine
 -- >>> g <- newStdGen
--- >>> let thetaSz = (P.read input1 :: Int)
--- >>> let distSz = (P.read input2 :: Int) 
--- >>> let noiseLevel = (P.read input3 :: Float)
--- >>> let snpImage :: Image VU Y Word16
--- >>>     snpImage = snp img thetaSz distSz noiseLevel g
--- >>> writeImage "images/yield_snp.png" (toImageRGB snpImage)
+-- >>> let noiseLevel = (P.read input1 :: Float)
+-- >>> let snpImage :: Image VU Y Double
+-- >>>     snpImage = saltAndPepper img noiseLevel g
+-- >>> writeImage "images/yield_snp.png" snpImage
 --
-snp
-  :: forall arr e cs . (MArray arr Y Double, IP.Array arr Y Double, IP.Array arr Y Word16, MArray arr Y Word16)
+saltAndPepper
+  :: forall arr e cs . (MArray arr Y Double, IP.Array arr Y Double)
   => Image arr Y Double
-  -> Int    -- ^ width of output image
-  -> Int    -- ^ height of output image
   -> Float  -- ^ Noise Intensity -> Domain : (0, 1)
   -> StdGen -- ^ Instance of RandomGen
-  -> Image arr Y Word16
-snp image thetaSz distSz noiseLevel gen  = I.map (fmap toWord16) (accBin gen)
+  -> Image arr Y Double
+saltAndPepper image noiseLevel = accBin
  where
    widthMax, heightMax, noiseIntensity :: Int
    widthMax = ((rows image) - 1)
@@ -63,19 +56,14 @@ snp image thetaSz distSz noiseLevel gen  = I.map (fmap toWord16) (accBin gen)
 
    accBin :: StdGen -> Image arr Y Double
    accBin g = runST $ 
-      do arr <- I.new (thetaSz, distSz)   
+      do arr <- I.thaw image  
          let coords = take (noiseIntensity + 1) (randomCoords g widthMax heightMax)
-         forM_ [0 .. widthMax] $ \x -> do
-           forM_ [0 .. heightMax] $ \y -> do
-             let px = I.index image (x, y)   
-             I.write arr (x, y) px
          forM_ coords $ \i -> do
            let a :: Int
                a = uncurry (+) i        
            if (a `mod` 2 == 0)
               then do let px =  0
-                      I.write arr i (PixelY (fromIntegral px))
-              else do let px = 255
-                      I.write arr i (PixelY (fromIntegral px))
+                      I.write arr i px
+              else do let px = 1.0
+                      I.write arr i px
          freeze arr
-
